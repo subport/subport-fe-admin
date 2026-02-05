547578181d4c415865bcebf5788faac89129ee81
yonghyeonpark <llyyoo93@gmail.com>
2026-02-05 18:17:47 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="d-flex" style="min-height: 100vh"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+.content-area {
+  background-color: #242429;
+}
+</style>
