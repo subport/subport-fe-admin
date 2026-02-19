80f4b83b9bbe8f50d540708a509b1a096916d084
yonghyeonpark <llyyoo93@gmail.com>
2026-02-20 04:17:21 +0900

    feat: 쿠키 기반 요청 API에 withCredentials 설정

---
+++ src/api/auth.ts
@@ -0,0 +1,20 @@
+import api from '.';
+import type { LoginRequest, TokenResponse } from './types';
+
+const BASE = '/auth';
+
+export function login(data: LoginRequest) {
+  return api.post<TokenResponse>(`${BASE}/login`, data, {
+    withCredentials: true,
+  });
+}
+
+export function refresh() {
+  return api.post<TokenResponse>(
+    `${BASE}/refresh`,
+    {},
+    {
+      withCredentials: true,
+    },
+  );
+}
