faa23d7b37148467cb01191e52a85a2504856e1f
yonghyeonpark <llyyoo93@gmail.com>
2026-02-21 18:51:33 +0900

    refactor: 회원 목록 페이지 날짜 포맷 수정

--- src/views/member/MemberListView.vue
+++ src/views/member/MemberListView.vue
@@ -523,7 +523,7 @@ function formatDate(isoString: string): string {
   const y = date.getFullYear();
   const m = String(date.getMonth() + 1).padStart(2, '0');
   const d = String(date.getDate()).padStart(2, '0');
-  return `${y}.${m}.${d}`;
+  return `${y}-${m}-${d}`;
 }
 </script>
 
